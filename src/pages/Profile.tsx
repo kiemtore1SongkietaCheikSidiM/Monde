090029fb28359245f93ca304dff41ddbb23fb2e6
unknown <skiemto>
2026-07-14 18:33:39 +0000

    user dans Profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,9 @@
 /* Here I import all the package I will use */
 import { MdModeEdit,MdDelete } from "react-icons/md"
 import { FaRegUserCircle } from "react-icons/fa";
+import { auth } from "../Firebase/firebase";
 
-
-let name: string = "Salimata Sawadogo"
-let email: string ='salimata47sawadogo@gmail.com'
+const user = auth.currentUser
 const Profile = () => {
     return (
         
@@ -14,8 +13,8 @@ const Profile = () => {
 
                 <div className="flex flex-col items-center">
                     <FaRegUserCircle className="w-25 h-25"/>
-                    <h5 className="mb-0.5 text-xl font-semibold tracking-tight text-heading">{name}</h5>
-                    <span className="text-sm text-body">{email}</span>
+                    <h5 className="mb-0.5 text-xl font-semibold tracking-tight text-heading">{user?.displayName}</h5>
+                    <span className="text-sm text-body">{user?.email}</span>
                     <div className="flex mt-4 md:mt-6 gap-4">
                         <button type="button" className="inline-flex items-center text-slate-700 dark:text-white bg-blue-400 box-border border border-transparent hover:bg-blue-600 focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none rounded-2xl">
                             <MdModeEdit className="w-6 h-6 "/>
